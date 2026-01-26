e5fbfc6b4cdd4c6fcc47577760bac440329c7f89
Camilo González <camilo.gonzalez@check24.es>
2026-01-26 16:50:39 +0100

    Added keymaps for quicklist

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,3 +1,6 @@
 vim.keymap.set('n', '<leader>yc', function()
   vim.fn.setreg('+', vim.fn.fnamemodify(vim.fn.expand '%:p', ':.'))
 end, { desc = 'Cop[y] relative [c]urrent file path (from cwd) to clipboard' })
+
+vim.keymap.set('n', '<leader>qn', vim.cmd.cnext, { desc = '[N]ext quick fix item' })
+vim.keymap.set('n', '<leader>qp', vim.cmd.cprev, { desc = '[P]revious quick fix item' })
