9d92dc9bc88ed8695e6a40a7e0f2dce6cf37cb92
Camilo González <camilo.gonzalez@check24.es>
2026-01-26 16:50:47 +0100

    Exclude copilot from .env files

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
+  pattern = { '.env', '.env.*' },
+  callback = function()
+    vim.b.copilot_enabled = false
+  end,
+})
+
 return {
   'github/copilot.vim',
   opts = {},
