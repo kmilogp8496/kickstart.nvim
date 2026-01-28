6a4d4a4ec97658285a895a4bcb18101b8dff64b8
Camilo González <camilo.gonzalez@check24.es>
2026-01-28 17:33:42 +0100

    Added timestamp command

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -4,3 +4,7 @@ end, { desc = 'Cop[y] relative [c]urrent file path (from cwd) to clipboard' })
 
 vim.keymap.set('n', '<leader>qn', vim.cmd.cnext, { desc = '[N]ext quick fix item' })
 vim.keymap.set('n', '<leader>qp', vim.cmd.cprev, { desc = '[P]revious quick fix item' })
+
+-- Use InsertTimestamp command with keymap
+vim.keymap.set('i', '<C-t>', '<Cmd>InsertTimestamp<CR>', { desc = 'Insert current Unix timestamp at cursor position' })
+vim.keymap.set('n', '<leader>tc', '<Cmd>InsertTimestamp<CR>', { desc = 'Insert [C]urrent Unix [T]imestamp at cursor position' })
